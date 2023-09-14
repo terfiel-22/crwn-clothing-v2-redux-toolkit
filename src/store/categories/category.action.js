2c58106751c16bb1c218f7748824de28fa00d974
terfiel-22 <dietherfiel@gmail.com>
2023-09-14 19:35:10 +0800

    Categories to createSlice

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,2 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
-import { createAction } from '../../utils/reducer/reducer.utils';
-
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+import { categoriesSlice } from "./category.reducer";
+export const { setCategories } = categoriesSlice.actions;
