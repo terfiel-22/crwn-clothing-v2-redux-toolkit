b25f5fbd3ba410cc561590e49a6f3f16b1bc8a74
terfiel-22 <dietherfiel@gmail.com>
2023-09-15 11:26:07 +0800

    Redux Toolkit Redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
-// import { store, persistor } from "./store/store";
-import { store } from "./store/store";
-// import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,43 +1,25 @@
-// import {
-//   compose,
-//   legacy_createStore as createStore,
-//   applyMiddleware,
-// } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
 const middleWares = [!import.meta.env.PROD && logger].filter(Boolean);
 
-// const composeEnhancer =
-//   (!import.meta.env.PROD &&
-//     window &&
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-//   compose;
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   blacklist: ["user"],
-// };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-// export const store = createStore(
-//   persistedReducer,
-//   undefined,
-//   composedEnhancers
-// );
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(middleWares),
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
